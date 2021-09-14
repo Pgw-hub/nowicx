75c22e4b928712e40d3f4e8f2a08e7db7aaf5987
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2021-09-15 01:54:37 +0900

    .

--- pset1/printfunc.cpp
+++
@@ -1,9 +0,0 @@
-// file: printfunc.cpp // C++ for C Coders & Data Structures // Lecture note by idebtor@gmail.com 
-#include <iostream>
-
-void printfunc(int n, char *names[]) {
-
-    for (int i = 0; i < n; i++)
-       std::cout << "Hello " << names[i] << "!" << std::endl;
-
-}
