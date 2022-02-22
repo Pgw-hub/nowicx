d0071816b5b73c0f21f61039dad2f5784e568d06
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-02-22 19:35:51 +0900

    char,char*,char[],string,string* differentiate

--- labs/personal/Chapter1/new.cpp
+++ labs/personal/Chapter1/new.cpp
@@ -8,19 +8,32 @@ int main(const int argc, char** argv){
 
 int* pi = new int;//uninitialized
 int* pi2 = new int();//initialized to 0
-int* pi3 = new int[7]();// 7 ints are initialized to 0
+int* pi3 = new int[7];// 7 ints are initialized to 0
 
+//문자열
 string* pi4 = new string("hello");
 string* pi5 = new string[7]();
-// string* pi6 = new string[2]{"a","the"}; //brace{} need to be used for initializing the array type of string;
+char* pi6 = new char[7]();//brace{} need to be used for initializing the array type of string;depending on version
+string pi7[2];
+
+
+pi6 = "ThankYou";
+// strcpy(pi6,"hello");
+pi7[0] = "안녕하세요";
 
 cout << pi[0] <<endl;
 cout << pi2[0] <<endl;
 cout << pi3[0] <<endl;
-cout << pi4[0][1] <<endl;
+cout << "pi4 :스트링 동적 할당입니다 > " <<pi4 <<endl;
+cout << "pi5 : string 동적 배열입니다 >" <<pi5[0] << endl;
 // cout << pi5 <<endl; // error
-cout << pi5[0] << endl;
-// cout << pi6 <<endl;
+cout <<" pi6 : char* 문자열입니다 >"<< pi6 <<endl;
+cout <<" pi6 : char* 문자열입니다 + 1 >"<< pi6+1 <<endl;
+cout <<" pi6 : char* 문자열입니다 + 2 >"<< pi6+2 <<endl;
+// *pi6 = 'O';수정불가.
+// cout <<" pi6 : char* 문자열 변경 후>"<< pi6 <<endl;
+cout << "pi7 :스트링 정적배열입니다. >" << pi7 <<endl;
+
 // cout << pi6[1][2] << endl;
 
 delete pi;
